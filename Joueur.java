173df20866e99bfc9ed3e1ee3ac1b1063739ed7f
oumaimaElkoufi <32762378+oumaimaElkoufi@users.noreply.github.com>
2017-11-11 00:37:40 +0100

    Add files via upload

--- Joueur.java
+++ Joueur.java
@@ -4,6 +4,11 @@ public class Joueur {
 	
 	private Lettre[] jeu = new Lettre[7];
 	private int score ;
+	private Plateau p = new Plateau();
+	private Lettre l = new Lettre();
+	private int multLettre;
+	private int multMot;
+	
 	
 	public Joueur() {
 		score = 0;
@@ -44,6 +49,67 @@ public class Joueur {
 		return false;
 	}
 	
+	
+	
+	/**
+	 * calculer le score d'un mot
+	 * @return le score du mot placé.
+	 */
+	
+	public int calculpoint(String mot) {
+		
+		for (int i = 0; i< mot.length(); i++) {
+			
+			char c = mot.charAt(i); 
+			int ch = (int)c;  
+			
+			l.initialisationLettre(ch);
+			
+			if(ch==l.getLettre()) {
+				
+				for (int k=0 ; k<15 ; k++) {
+					for (int j=0 ; j<15 ; j++)
+					{
+						if(p.getPlateau_cases_spe().equals(1)) {
+							multLettre = 2;
+						}
+						else if(p.getPlateau_cases_spe().equals(2)) {
+							multLettre = 3;
+						}else multLettre = 1;
+						
+						if(p.getPlateau_cases_spe().equals(3)) {
+							multMot = 2;
+						}else if(p.getPlateau_cases_spe().equals(4)){
+							multMot = 3;
+						}else multMot = 1;
+					}
+				}
+				
+				score += l.getScore() * multLettre;
+			
+		} 
+			score = score* multMot;
+	}
+		
+		
+		return score;
+	}
+	
+	
+	/**
+	 * placer une lettre sur le plateau
+	 */
+	
+	public void placerLettre(int indice , int x , int y , Plateau p) 
+	{
+		if(p.getPlateau(x, y).getLettre() ==' ') {
+			p.getPlateau(x, y).setLettre(jeu[indice].getLettre());
+			jeu[indice].setLettre(' ');
+		}
+		
+	}
+	
+	
 
 	/**
 	 * Getter and Setter
@@ -64,7 +130,15 @@ public class Joueur {
 	public void setScore(int score) {
 		this.score = score;
 	}
-	
+
+	public Lettre getL() {
+		return l;
+	}
+
+	public void setL(Lettre l) {
+		this.l = l;
+	}
+
 	
 	
 }
